26c652cec58722ac911995ef58ef7d3c72904d0f
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-09-01 21:11:35 -0400

    modifica vista Profile con componentes Articulos y FormPost

--- src/components/Articulos.jsx
+++ src/components/Articulos.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useContext } from 'react'
+import { TecnoContext } from '../context/TecnoContext'
+import { Container, Row } from 'react-bootstrap'
+import ProductCard from '../components/ProductCard'
 
-const Articulos = () => {
+const Articulos = ({ articulo, eliminarArticulo }) => {
+  const { addAndRemoveFavorite } = useContext(TecnoContext)
+
   return (
-    <div>Articulos</div>
+    <Container>
+      <Row>
+        <ProductCard
+          id={articulo.id}
+          titulo={articulo.titulo}
+          img={articulo.img}
+          descripcion={articulo.descripcion}
+          marca={articulo.marca}
+          precio={articulo.precio}
+          addAndRemoveFavorite={addAndRemoveFavorite}
+          eliminarArticulo={eliminarArticulo}
+        />
+      </Row>
+    </Container>
   )
 }
 
