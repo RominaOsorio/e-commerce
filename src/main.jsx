378b2e9894f2f5f1946efeb8d1faee27a08385ac
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-09-01 20:19:30 -0400

    config main.jsx y se crea archivo products.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { HashRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>
+      <Auth0Provider
+        domain='rominadeveloper.us.auth0.com'
+        clientId='raMWUtwhmenJ0Fwqlp4FilGSaB42gLWP'
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </HashRouter>
+  </React.StrictMode>
 )
