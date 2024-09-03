5f70a5ea861f53244c8132808f82728d1fb61e76
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-09-02 22:18:34 -0400

    Elimina Auth0 y crea Authcontext para definir rutas publicas y privadas

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,17 @@ import App from './App.jsx'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { HashRouter } from 'react-router-dom'
-import { Auth0Provider } from '@auth0/auth0-react'
 import TecnoProvider from './context/TecnoContext.jsx'
+import AuthProvider from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <Auth0Provider
-        domain='rominadeveloper.us.auth0.com'
-        clientId='raMWUtwhmenJ0Fwqlp4FilGSaB42gLWP'
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-      >
+      <AuthProvider>
         <TecnoProvider>
           <App />
         </TecnoProvider>
-      </Auth0Provider>
+      </AuthProvider>
     </HashRouter>
   </React.StrictMode>
 )
